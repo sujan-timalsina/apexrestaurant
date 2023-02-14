332b07e360b30eeb92cd62fe8683704bd769440c
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-02-14 18:55:20 +0545

    CRUD operation functionality using Mvc as a UI

---
+++ ApexRestaurant.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,140 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using ApexRestaurant.Mvc.Models;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+public class CustomerController : Controller
+{
+    private string? baseUri;
+    public CustomerController(IConfiguration iConfig)
+    {
+        // Get baseUri of Web API from appsettings.json > ApiBaseUrl
+        baseUri = iConfig.GetValue<string>("ApiBaseUrl");
+    }
+
+    // GET: Customer
+    public ActionResult Index()
+    {
+        IEnumerable<CustomerViewModel>? customers = null;
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(baseUri ?? throw new InvalidOperationException("baseUri is null"));
+            client.DefaultRequestHeaders.Add("accept", "application/json");
+            var responseTask = client.GetAsync("api/customer");
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var apiResponse = result.Content.ReadAsStringAsync();
+                apiResponse.Wait();
+                customers = JsonConvert.DeserializeObject<IList<CustomerViewModel>>(apiResponse.Result);
+            }
+            else //web api sent error response 
+            {
+                customers = Enumerable.Empty<CustomerViewModel>();
+                ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+            }
+        }
+        return View(customers);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(CustomerViewModel customer)
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(baseUri ?? throw new InvalidOperationException("baseUri is null"));
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            //HTTP POST
+            customer.EnrollDate = DateTime.Now;
+            customer.CreatedBy = "admin";
+            customer.CreatedOn = DateTime.Now;
+            customer.UpdatedBy = "admin";
+            customer.UpdatedOn = DateTime.Now;
+
+            var customerJson = JsonConvert.SerializeObject(customer);
+            var postTask = client.PostAsync("api/customer", new StringContent(customerJson, Encoding.UTF8, "application/json"));
+            postTask.Wait();
+
+            var result = postTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+        return View(customer);
+    }
+
+    public async Task<ActionResult> Edit(int id)
+    {
+        CustomerViewModel? customer = null;
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(baseUri ?? throw new InvalidOperationException("baseUri is null"));
+            //HTTP GET
+            var result = await client.GetAsync("api/customer/" + id.ToString());
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = await result.Content.ReadAsStringAsync();
+                customer = JsonConvert.DeserializeObject<CustomerViewModel>(readTask);
+            }
+        }
+        return View(customer);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(CustomerViewModel customer)
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(baseUri ?? throw new InvalidOperationException("baseUri is null"));
+
+            //HTTP PUT
+            var customerJson = JsonConvert.SerializeObject(customer);
+            var content = new StringContent(customerJson, Encoding.UTF8, "application/json");
+            var putTask = client.PutAsync("api/customer", content);
+            putTask.Wait();
+            var result = putTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        return View(customer);
+    }
+
+    [HttpDelete]
+    public ActionResult Delete(int id)
+    {
+        Console.WriteLine("Delete: " + id);
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(baseUri ?? throw new InvalidOperationException("baseUri is null"));
+            //HTTP DELETE
+            var deleteTask = client.DeleteAsync("api/customer/" + id.ToString());
+            deleteTask.Wait();
+            var result = deleteTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        return RedirectToAction("Index");
+    }
+
+}
---
+++ ApexRestaurant.Mvc/Models/CustomerViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ApexRestaurant.Mvc.Models
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Address { get; set; }
+        public string? PhoneRes { get; set; }
+        public required string PhoneMob { get; set; }
+        public DateTime EnrollDate { get; set; }
+        public bool IsActive { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
